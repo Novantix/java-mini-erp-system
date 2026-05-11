6e99c5e0f595999fe47b01164a25f35a2e006661
Anil <panil.spec@gmail.com>
2026-05-11 16:22:28 +0530

    added admin and file service classes

---
+++ AdminService.java
@@ -0,0 +1,10 @@
+
+public class AdminService {
+
+    public void adminMenu() {
+        System.out.println("Admin Panel");
+        System.out.println("1. View Reports");
+        System.out.println("2. Manage Users");
+        System.out.println("3. Exit");
+    }
+}
