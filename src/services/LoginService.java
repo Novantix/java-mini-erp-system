b3b540de5b25bf5542d9daca967334288c8dd5bb
C Mohan <mohanchb123@gmail.com>
2026-05-11 18:22:16 +0530

    added LoginService.java file

--- src/services/LoginService.java
+++ src/services/LoginService.java
@@ -1,29 +1,34 @@
-package services;
+package service;
 
 import java.util.Scanner;
 import models.User;
-
 public class LoginService {
-
-    public void login(User user) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Login Username: ");
+    Scanner sc = new Scanner(System.in);
+    public User registerUser() {
+        System.out.println("===== USER REGISTRATION =====");
+        System.out.print("Create Username : ");
+        String username = sc.nextLine();
+        System.out.print("Create Password : ");
+        String password = sc.nextLine();
+        System.out.print("Enter Role (Admin/Employee) : ");
+        String role = sc.nextLine();
+        User user = new User(username, password, role);
+        System.out.println("Registration Successful");
+        return user;
+    }
+    public User login(User registeredUser) {
+        System.out.println("\n===== LOGIN PAGE =====");
+        System.out.print("Enter Username : ");
         String loginUsername = sc.nextLine();
-
-        System.out.print("Login Password: ");
+        System.out.print("Enter Password : ");
         String loginPassword = sc.nextLine();
-
-        if (user.getUsername().equals(loginUsername)
-                && user.getPassword().equals(loginPassword)) {
-
-            user.setLoggedIn(true);
+        if (registeredUser.getUsername().equals(loginUsername)
+                && registeredUser.getPassword().equals(loginPassword)) {
             System.out.println("Login Successful");
-
+            return registeredUser;
         } else {
-
             System.out.println("Invalid Username or Password");
+            return null;
         }
     }
 }
