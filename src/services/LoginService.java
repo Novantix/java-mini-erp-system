a54c87a672682b6265b49110603976dd108f89da
C Mohan <mohanchb123@gmail.com>
2026-05-11 17:36:32 +0530

    add LoginService.java file

--- src/services/LoginService.java
+++ src/services/LoginService.java
@@ -1,5 +1,29 @@
 package services;
 
+import java.util.Scanner;
+import models.User;
+
 public class LoginService {
-    
+
+    public void login(User user) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Login Username: ");
+        String loginUsername = sc.nextLine();
+
+        System.out.print("Login Password: ");
+        String loginPassword = sc.nextLine();
+
+        if (user.getUsername().equals(loginUsername)
+                && user.getPassword().equals(loginPassword)) {
+
+            user.setLoggedIn(true);
+            System.out.println("Login Successful");
+
+        } else {
+
+            System.out.println("Invalid Username or Password");
+        }
+    }
 }
