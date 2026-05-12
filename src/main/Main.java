8e588d99b44a6c801dd83fcec446762cc8aa5a1f
C Mohan <mohanchb123@gmail.com>
2026-05-12 15:52:23 +0530

    Added Main.java File

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,25 @@
-<<<<<<< HEAD
-=======
 package main;
->>>>>>> main
-
+import models.User;
+import services.LoginService;
+import services.RoleService;
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Java Mini ERP System");
+        LoginService loginService = new LoginService();
+        RoleService roleService = new RoleService();
+        User registeredUser = loginService.registerUser();
+        User loginUser = loginService.login(registeredUser);
+        if (loginUser != null) {
+            if (roleService.isAdmin(loginUser.getRole())) {
+                System.out.println("Welcome Admin");
+                System.out.println("Admin Access Granted");
+            } else if (roleService.isEmployee(loginUser.getRole())) {
+                System.out.println("Welcome Employee");
+                System.out.println("Employee Access Granted");
+            } else {
+                System.out.println("Invalid Role");
+            }
+        } else {
+            System.out.println("Login Failed");
+        }
     }
 }
