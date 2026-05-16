6d73d3409060da31544a02dff4c0487d648b0c85
Novantix <nspv2.o@gmail.com>
2026-05-16 15:30:10 +0530

    Update Leave.java

--- src/models/Leave.java
+++ src/models/Leave.java
@@ -11,12 +11,14 @@ public class Leave {
     private String reason;
     private String status;
 
+    // Constructor
     public Leave(
             int employeeId,
             String employeeName,
             LocalDate fromDate,
             LocalDate toDate,
-            String reason) {
+            String reason
+    ) {
 
         this.employeeId = employeeId;
         this.employeeName = employeeName;
@@ -26,22 +28,49 @@ public class Leave {
         this.status = "PENDING";
     }
 
+    // Getters
     public int getEmployeeId() {
         return employeeId;
     }
 
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public LocalDate getFromDate() {
+        return fromDate;
+    }
+
+    public LocalDate getToDate() {
+        return toDate;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // Approve Leave
     public void approveLeave() {
         this.status = "APPROVED";
+    }
+
+    // Reject Leave
+    public void rejectLeave() {
+        this.status = "REJECTED";
     }
 
     @Override
     public String toString() {
 
-        return "\nEmployee ID   : " + employeeId
+        return "Employee ID : " + employeeId
                 + "\nEmployee Name : " + employeeName
-                + "\nFrom Date     : " + fromDate
-                + "\nTo Date       : " + toDate
-                + "\nReason        : " + reason
-                + "\nStatus        : " + status;
+                + "\nFrom Date : " + fromDate
+                + "\nTo Date : " + toDate
+                + "\nReason : " + reason
+                + "\nStatus : " + status;
     }
 }
